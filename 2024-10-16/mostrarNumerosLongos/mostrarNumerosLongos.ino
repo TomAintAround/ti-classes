f52139be88e636910a2b37f0b2cb32094645e506
TomAintAround <tcorreia2005@proton.me>
2024-10-23 11:57:45 +0100

    fix(2024-10-16/mostrarNumerosLongos): actually add the code

--- 2024-10-16/mostrarNumerosLongos/mostrarNumerosLongos.ino
+++ 2024-10-16/mostrarNumerosLongos/mostrarNumerosLongos.ino
@@ -1,7 +1,7 @@
-// Materiais: 4 220 ohm resistores, 4 leds, breadboard, botão
+// Materiais: 4 220 ohm resistores, 4 leds, breadboard
 // Exercício 2.10
 
-long numero = 213452345;
+long numero = 456787654;
 byte minLed = 2;
 byte maxLed = 5;
 
@@ -9,7 +9,16 @@ void setup() {
   for (byte pin = minLed; pin <= maxLed; pin++) pinMode(pin, OUTPUT);
   Serial.begin(9600);
 
-  for (byte nibble = 0); nibble < 8; nibble++) {
+  Serial.println(numero, BIN);
+
+  for (byte nibble = 0; nibble < 8; nibble++) {
+    byte nibbleLED = (numero >> (4 * nibble)) & 0xF;
+    for (byte dislocator = 0; dislocator < 4; dislocator++) {
+      byte selectedLED = minLed + dislocator;
+      bool algarismoON = (nibbleLED >> dislocator) & 1;
+      digitalWrite(selectedLED, algarismoON);
+    }
+    delay(1000);
   }
 }
 

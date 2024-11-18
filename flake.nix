96549286907c4843f5d1ddc2d12434920882bab0
TomAintAround <tcorreia2005@proton.me>
2024-11-18 14:45:22 +0000

    chore: add flake.nix file

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+    description = "TI shell";
+
+    inputs = {
+        nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+    };
+
+    outputs = inputs: let
+        system = "x86_64-linux";
+        pkgs = import inputs.nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+        };
+    in {
+        devShells.${system}.default =
+            pkgs.mkShell {
+                packages = with pkgs; [
+                    python3
+                    python312Packages.tkinter
+                    python312Packages.pyserial
+                ];
+            };
+    };
+}

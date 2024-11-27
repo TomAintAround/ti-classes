5635d628b904e5356bb8f8bfb9eb65c43e75fa5d
TomAintAround <tcorreia2005@proton.me>
2024-11-27 12:29:46 +0000

    fix(2024-11-20): make each color progressively turn on and then off

--- 2024-11-20/intensidadeLED/intensidadeLED.ino
+++ 2024-11-20/intensidadeLED/intensidadeLED.ino
@@ -1,25 +1,28 @@
-// Exercício 3.5
 // Materiais: breaboard, 4 fios, 1 LED RGB, 3 resistores 220 ohms
 
 const byte leds[3] = {9, 10, 11};
-const byte quantidadeLeds = sizeof(leds)/sizeof(byte);
-byte intensidade = 255;
+const size_t quantidadeLeds = sizeof(leds)/sizeof(byte);
+byte intensidade = 0;
 
 void setup() {
-    for (byte led = 0; led < quantidadeLeds; led++) pinMode(leds[led], OUTPUT);
+    Serial.begin(9600);
+    for (byte led = 0; led < quantidadeLeds; led++) {
+        pinMode(leds[led], OUTPUT);
+        analogWrite(leds[led], 255);
+    }
 }
 
 void loop() {
-    for (byte led = 0; led < quantidadeLeds; led++) {
-        while (intensidade >= 0) {
-            analogWrite(leds[led], intensidade);
+    for (int led = quantidadeLeds - 1; led >= 0; led--) {
+        while (intensidade < 255) {
+            analogWrite(leds[led], 255 - intensidade);
+            delay(3);
+            intensidade++;
+        }
+        while (intensidade > 0) {
+            analogWrite(leds[led], 255 - intensidade);
             delay(3);
             intensidade--;
-        }
-        while (intensidade < 256) {
-            intensidade++;
-            analogWrite(leds[led], intensidade);
-            delay(3);
         }
     }
 }
